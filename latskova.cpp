8e2dee35a5372cc5ebb28acf4dd942e2767582e0
Sofia Latskova <sofialatskova.2018@gmail.com>
2026-09-11 19:08:15 +0700

    fix: исправлены ошибки

--- latskova.cpp
+++ latskova.cpp
@@ -1,14 +1,16 @@
 ﻿#include "latskova.h"
 #include <cmath>
 #include <iostream>
+#include <stdexcept>
 using namespace std;
+const double G = 9.81;
 
 double potentialEnergy(double m, double h) {
 	if (m < 0)throw invalid_argument("Масса не может быть отрицательной!");
-	return m * h * g;
+	return m * h * G;
 }
 
 double heightFromEnergy(double E, double m) {
-	if (m = < 0)throw invalid_argument("Масса не может быть отрицательной или равной нулю!");
-	return E / (m * g);
+	if (m  <= 0)throw invalid_argument("Масса не может быть отрицательной или равной нулю!");
+	return E / (m * G);
 }
