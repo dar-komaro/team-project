de2cc09b21bf9506dd57d49c1a3f1bdee59d5091
Sofia Latskova <sofialatskova.2018@gmail.com>
2026-09-11 16:05:24 +0700

    feat: функции перенесены варианта 25

--- latskova.h
+++ latskova.h
@@ -3,5 +3,5 @@
 
 // Функции варианта 25 (Лацкова С. А.)
 double potentialEnergy(double m, double h);
-heightFromEnergy(double E, double m);
+double heightFromEnergy(double E, double m);
 #endif // LATSKOVA_H
