6518bd1d5a757e4beea5daef7df33607d54055d0
Sofa Parshukova <parshukovasof@yandex.ru>
2026-09-11 15:38:46 +0700

    feat: созданы файлы функций варианта 63

---
+++ parshukova.cpp
@@ -0,0 +1,11 @@
+#include "parshukova.h"
+
+// Километры → мили
+double kmToMiles(double km) {
+    return km * 0.621371;
+}
+
+// Мили → километры
+double milesToKm(double miles) {
+    return miles * 1.609344;
+}
---
+++ parshukova.h
@@ -0,0 +1,8 @@
+#ifndef PARSHUKOVA_H
+#define PARSHUKOVA_H
+
+// Функции варианта 63 (Паршукова С.М.)
+double kmToMiles(double km);
+double milesToKm(double miles);
+
+#endif // PARSHUKOVA_H
