aaf6368d9b9a63c381998235242e4f3feb88389b
Darya Komarovskaya <komarod07@gmail.com>
2026-09-11 15:19:47 +0700

    Подключен komarovskaya.h варианта 27 к главному меню программы

---
+++ komarovskaya.h
@@ -0,0 +1,9 @@
+#ifndef KOMAROVSKAYA_H
+#define KOMAROVSKAYA_H
+
+// Функции варианта 27 (Комаровская Д. Е.)
+double density(double m, double V);
+double massFromDensity(double rho, double V);
+double volumeFromDensity(double m, double rho);
+
+#endif // KOMAROVSKAYA_H
